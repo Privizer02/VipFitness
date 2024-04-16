2234760936e08d7eefc892793a16bea056965a82
Privizer02 <milanprivizer2002@gmail.com>
2024-04-16 17:13:52 +0200

    Functional sidebar

--- src/app/core/nav/nav.component.ts
+++ src/app/core/nav/nav.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
 })
-export class NavComponent {}
+export class NavComponent {
+  ngOnInit() {
+    this.sideBar();
+  }
+
+  sideBar() {
+    console.log('work');
+    const toggle = <HTMLElement>document.querySelector('.main');
+    toggle.classList.toggle('active');
+  }
+}
